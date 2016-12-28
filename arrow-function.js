c3dd49ecc400a2244e5a52095a53f49c8e589065
Mucahit INEL <mucahit.inel@hotmail.com>
2016-12-28 20:04:51 +0300

    why arrow functions not access to this

--- arrow-function.js
+++ arrow-function.js
@@ -2,7 +2,7 @@
 //multiply is old method for object define object functions
 //times is new way for declarations
 //this.number is undefined in the product!
-
+//Arrow functions do not define ("bind") their own this.
 let obj = {
     number: 2,
     multiply: function(n) {
@@ -11,9 +11,14 @@ let obj = {
     times(n) {
         return this.number * n;
     },
-    product: n => this.number * n
+    product: n => this.number * n,
+    get: () => {
+    console.log(this.number);
+    return this.number; // represents global object 'Window', therefore 'this.a' returns 'undefined'
+    }
 };
 
 console.log(obj.multiply(2)); //4
 console.log(obj.times(3));    //6
 console.log(obj.product(4));  //NaN
+console.log(obj.get());
